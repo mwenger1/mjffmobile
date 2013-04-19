d409f6cb4ce0d3770cab8cf335c11805fdf19306
Michael Wenger <michaelwenger27@gmail.com>
2013-04-19 10:43:22 -0400

    Added active css state for View Full Site link

--- include/bottom.php
+++ include/bottom.php
@@ -1,8 +1,7 @@
 </div>
 			<div id="footer" data-role="footer" style="background:#1D2627 url(''); height:2em;">
 				<div style="text-align:center";>
-<a href="https://www.michaeljfox.org" target="_blank" style="font-weight:normal; line-height:42px; color:white; text-decoration:underline;">VIEW FULL SITE</a>
-		
+					<a href="https://www.michaeljfox.org" target="_blank" class="viewFullSite">VIEW FULL SITE</a>
 				</div>
 			</div>
 		</div>
