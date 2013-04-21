93d8259479a9ae5537d173c2dda007ed01b08d58
Michael Wenger <michaelwenger27@gmail.com>
2013-04-21 17:02:43 -0400

    Various TeamFox Changes

--- teamfox.php
+++ teamfox.php
@@ -6,27 +6,49 @@
 			<div class="teamfoxFundraiseContent">
 				<h2 class="onepointthree">What is <span class="teamfoxRed">TEAM</span><span class="teamfoxOrange">FOX?</span></h2>
 				<p>Team Fox is the grassroots community fundraising program at The Michael J. Fox Foundation. Each year more than 1,600 Team Fox members worldwide turn their passions and interests into unique fundraising events and athletic feats.</p>
-				<p>To date, our members have raised nearly $20 million for Parkinson’s research. You too can make a difference. <a href="teamfox-signup.php">Sign up today!</a></p>
+				<img src="https://www.michaeljfox.org/files/blog/MJF_Marathon-Cheering-Section_2.jpg" style="width:100%;"/>
+				<p>To date, our members have raised nearly $20 million for Parkinson’s research. You too can make a difference. <!-- <a href="teamfox-signup.php">Sign up today!</a> --></p>
 			<div class="clear"></div>
 			
 			<hr/>
 			
-			<h2 class="onepointthree">Why join <span class="teamfoxRed">TEAM</span><span class="teamfoxOrange">FOX?</span></h2>
-			<p>All Team Fox members are equipped with a wealth of tools and resources, including:</p>
-			<ul class="reasonsTeamFox">
-				<li>An online fundraising page with a unique URL that can be customized with a personal story, event information and photo or video.</li>
-				<li>A Participant Center that serves as your fundraising headquarters; from there you can email friends and family members, track donations, share your story, and more.</li>
-				<li>The Team Fox Handbook, a 20+ page guide to help you maximize your fundraising and event planning efforts.</li>
-				<li>Access to the Team Fox logo, banners, and other collateral items to give your event legitimacy.</li>
-				<li>Assistance from the Team Fox staff and/or a Team Fox mentor.</li>
-				</ul>
-			<hr/>
+			<h2 class="onepointthree" style="margin-bottom:1em;">Why join <span class="teamfoxRed">TEAM</span><span class="teamfoxOrange">FOX?</span></h2>
+
+			<div class="ftfReasons">	
+				<div data-role="collapsible"  data-iconpos="right" data-inset="false" data-theme="b" data-content-theme="d">
+					<h3>What You Get</h3>
+					<ul>
+						<li>An online fundraising page with a unique URL that can be customized with a personal story, event information and photo or video.</li>
+						<li>A Participant Center that serves as your fundraising headquarters; from there you can email friends and family members, track donations, share your story, and more.</li>
+						<li>The Team Fox Handbook, a 20+ page guide to help you maximize your fundraising and event planning efforts.</li>
+						<li>Access to the Team Fox logo, banners, and other collateral items to give your event legitimacy.</li>
+						<li>Assistance from the Team Fox staff and/or a Team Fox mentor.</li>
+					</ul>
+				</div>
+				<div data-role="collapsible" data-iconpos="right" data-inset="false" data-theme="b" data-content-theme="d">
+					<h3>For Athletes</h3>
+					<ul>
+						<li>
+							Awaiting text.
+						</li>
+					</ul>
+				</div>
+				<div data-role="collapsible" data-iconpos="right" data-inset="false" data-theme="b" data-content-theme="d" style="margin-bottom:1em;">
+					<h3>For Community Fundraisers</h3>
+					<p>
+							Team Fox members are hosting garden walks, concerts, art auctions, car shows, and much more — all to help us speed a cure for Parkinson's. But the creativity doesn't end there. Let your talents and interests lead you to your own fundraising endeavor for Team Fox.
+						</p>
+				</div>
+			<a href="teamfox-signup.php" class="donateBtn" data-theme="a" data-role="button" >Sign Up</a>
+
+			</div>
+
 			<ul data-role="listview" data-inset="true" class="ui-li-icon" data-divider-theme="b">
 				<li role="heading" data-role="list-divider" data-swatch="a" >
-					Take Action
+					Other Ways To Get Involved
 				</li>
 				<li>
-					<a href="teamfox-signup.php" data-iconpos="right" data-theme="c" data-icon="arrow-r" transition="slide">Join Team Fox</a>
+					<a target="_blank" href="https://www.michaeljfox.org/get-involved/support-participant.html" data-iconpos="right" data-icon="arrow-r" data-transition="slide" data-prefetch>Donate to a Fundraiser</a>
 				</li>
 				<li>
 					<a href="events.php" data-iconpos="right" data-icon="arrow-r" data-transition="slide" data-prefetch>Find a Team Fox Event</a>
@@ -34,6 +56,10 @@
 				<li>
 					<a href="blog.php" data-iconpos="right" data-icon="arrow-r" data-transition="slide" data-prefetch>Read the Team Fox Blog</a>
 				</li>
+				<li>
+					<a href="foxtrialfinder.php" data-iconpos="right" data-icon="arrow-r" data-transition="slide" data-prefetch>Get Involved with Research</a>
+				</li>
+
 			</ul>
 			</div>
 				
